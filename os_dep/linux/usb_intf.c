a69c3f34a9b127d6c816ed280412ec527e1ae731
Taylor Buley <buley@outlook.com>
2025-12-29 15:18:54 -0800

    tp-link USB device support

--- os_dep/linux/usb_intf.c
+++ os_dep/linux/usb_intf.c
@@ -163,6 +163,7 @@ static struct usb_device_id rtw_usb_id_tbl[] = {
 
         /*=== TP-Link AX1800 ===*/
 	{USB_DEVICE_AND_INTERFACE_INFO(0x35bc, 0x0100, 0xff, 0xff, 0xff), .driver_info = RTL8852A},
+	{USB_DEVICE_AND_INTERFACE_INFO(0x35bc, 0x0108, 0xff, 0xff, 0xff), .driver_info = RTL8852A}, /* TP-Link 35bc:0108 */
 	{USB_DEVICE_AND_INTERFACE_INFO(USB_VENDOR_ID_TPLINK, 0x013f, 0xff, 0xff, 0xff), .driver_info = RTL8852A},
 	{USB_DEVICE_AND_INTERFACE_INFO(USB_VENDOR_ID_TPLINK, 0x0140, 0xff, 0xff, 0xff), .driver_info = RTL8852A},
 
